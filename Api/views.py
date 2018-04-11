c5633d82125043916fcde685cedb9ce88dfbceae
oronibrian <brianoroni6@gmail.com>
2018-04-11 13:39:23 +0300

    Modified view

--- Api/views.py
+++ Api/views.py
@@ -11,7 +11,7 @@ from .models import Job, Bid, Notification, GeoLocation, Wallet, Transactions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAdminUser
 from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
-from rest_framework .view import APIView
+from rest_framework.views import APIView
 from rest_framework .response import Response
 from rest_framework.permissions import AllowAny
 
