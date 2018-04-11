42b41d2d41deb991c34b1bdc365986709d241f23
oronibrian <brianoroni6@gmail.com>
2018-04-11 11:06:47 +0300

    permissions changed

--- vaqdemaj/settings.py
+++ vaqdemaj/settings.py
@@ -144,8 +144,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # # 'rest_framework.permissions.AllowAny',
+          # 'rest_framework.permissions.IsAuthenticated',
+
 
 
 
